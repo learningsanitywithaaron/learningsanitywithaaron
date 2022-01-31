0937517a8d1cce0cee0278b6936e6e5dc4c5a7f2
philip howley <howley.phil@gmail.com>
2022-01-31 15:46:42 -0500

    Update Navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -14,20 +14,20 @@ export default function Navbar() {
       <h2 className='h2 nav-subtitle text-center'>I Would Write About</h2>
       <ul className='nav-items'>
         <Link className='nav-item' to='/blog' activeClassName='active'>
-          Blog
+          Tech
         </Link>
-        <Link className='nav-item' to='/speaking' activeClassName='active'>
-          Talk
-        </Link>
-        <Link className='nav-item' to='/gold' activeClassName='active'>
-          Gold
-        </Link>
-        <Link className='nav-item' to='/silver' activeClassName='active'>
-          Silver
-        </Link>
-        <Link className='nav-item' to='/economics' activeClassName='active'>
-          Economics
-        </Link>
+        {/* <Link className='nav-item' to='/speaking' activeClassName='active'> */}
+        {/*   Talk */}
+        {/* </Link> */}
+        {/* <Link className='nav-item' to='/gold' activeClassName='active'> */}
+        {/*   Gold */}
+        {/* </Link> */}
+        {/* <Link className='nav-item' to='/silver' activeClassName='active'> */}
+        {/*   Silver */}
+        {/* </Link> */}
+        {/* <Link className='nav-item' to='/economics' activeClassName='active'> */}
+        {/*   Economics */}
+        {/* </Link> */}
       </ul>
     </nav>
   )
